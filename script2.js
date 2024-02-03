f237d7e968a613f4a0efe9f1ff676837b2c8d1aa
kodekartiki <kodekartiki03@gmail.com>
2024-02-03 19:28:10 +0530

    added

---
+++ script2.js
@@ -0,0 +1,31 @@
+for( let i=2; i<=10; i++)
+{
+    console.log(i*2)
+}
+// program 2
+for(let i=2; i<=20; i++)
+{
+    console.log(i)
+}
+
+//program 3
+for(let i=50; i>=5; i=i-5){
+    console.log(i)
+}
+//break statements with loop 
+//program 4
+
+for (let i=1; i<=5; i++){
+    if(i == 3){
+      break
+    }
+console.log(i)
+}
+// program 5
+for (let i =1; i<=5; i++)
+{
+    if(i==4){
+        break
+    }
+     console.log(i)
+}
